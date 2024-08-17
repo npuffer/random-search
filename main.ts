b131684dbaa8dbcc237a0cba3611adfeeb50bb24
npuffer <puffernc@gmail.com>
2024-08-18 09:24:42 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+let distance = 0
+basic.forever(function () {
+    distance = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distance < 6) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        basic.pause(1000)
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, randint(10, 100))
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, randint(10, 100))
+        basic.pause(1000)
+        maqueen.motorStop(maqueen.Motors.M1)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+})
